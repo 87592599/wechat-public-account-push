d7cdb9070fd4336934ff7dbc3e9dae14d33089d4
87592599 <135884935+87592599@users.noreply.github.com>
2024-04-06 20:26:10 +0800

    index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -55,7 +55,7 @@ const USER_CONFIG = {
 
   CALLBACK_USERS: [
     {
-      name: 'oGKR_6JutOQLwp5IC4rPPAjLSzmA',
+      name: 'oGKR_6EXCCG7_KESrDfVSJxuK0NI',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: '',
     }
